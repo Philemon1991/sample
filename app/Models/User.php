2322dd8ff85be847b1c79d9c7e300d8b54eb9585
philemon <philemon1991@gmail.com>
2018-11-09 08:31:16 +0000

    Finish user signup

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function gravatar($size = '100')
+    {
+        return 'https://note.youdao.com/yws/api/image/normal/1504835456871?userId=weixinobU7VjoKRQw6DBO3ztVWPF1B2LEI';
+    }
 }
